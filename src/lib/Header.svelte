f3a0a66f68f474ae75354df06157c1d71ffc1b55
ajgeiss0702 <ajgeiss72@gmail.com>
2023-02-19 10:41:22 -0700

    basics and header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,43 @@
+<style>
+    .head {
+        font-size: 2rem;
+        box-shadow: none;
+    }
+
+    img {
+        height: 1.5em;
+    }
+
+    .light-logo {
+        display: none;
+    }
+    .logo {
+        display: inline-block;
+    }
+    @media (prefers-color-scheme: dark) {
+        .light-logo {
+            display: inline-block;
+        }
+        .logo {
+            display: none;
+        }
+    }
+
+    span {
+        font-size: 0.7em;
+    }
+</style>
+<div class="w-full head px-5 card rounded-none m-0 flex">
+    <div class="grow">
+        <a href="/" class="hidden-link">
+            <img class="logo align-top" src="/kev.webp" alt="Kevin Geiss"/>
+            <img class="light-logo align-top" src="/kev-lighter.webp" alt="Kevin Geiss"/>
+            Kevin Geiss
+        </a>
+    </div>
+    <span class="self-center">
+        <a href="https://blog.astrophoenix.com/" class="hidden-link hoverable" target="_blank">
+            Blog
+        </a>
+    </span>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../theme.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+    import Header from "../lib/Header.svelte";
+</script>
+<div class="position-absolute top-0 w-full">
+    <Header/>
+</div>
+<slot />
