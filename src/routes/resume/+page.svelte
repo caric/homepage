963cc823ab8f79d9b28df19526ef66926cffa115
ajgeiss0702 <ajgeiss72@gmail.com>
2023-03-14 15:16:57 -0700

    resume page

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,1 +1,30 @@
-TODO
+<h1>Resume</h1>
+<br>
+<p>
+    I hold a Master of Science degree in
+    <a href="http://www.cs.arizona.edu/" target="_blank" rel="noreferrer">Computer Science</a>
+    from the
+    <a href="http://www.arizona.edu/" target="_blank" rel="noreferrer">University of Arizona</a>,
+    as well as Bachelor of Science degrees in
+    <a href="http://computerscience.missouristate.edu" target="_blank" rel="noreferrer">Computer Science</a>
+    and
+    <a href="http://math.missouristate.edu" target="_blank" rel="noreferrer">Math</a>
+    from <a href="http://www.missouristate.edu" target="_blank" rel="noreferrer">Missouri State University</a>.
+</p>
+<br>
+<br>
+My resume is available in several formats:<br>
+<ul>
+    <li><a href="/resume/geiss.pdf">PDF</a></li>
+    <li><a href="/resume/geiss.dvi">TeX (dvi)</a></li>
+    <li><a href="/resume/geiss.ps">PostScript</a></li>
+    <li><a href="/resume/geiss.txt">Plain Text</a></li>
+</ul>
+
+
+<style>
+    ul {
+        list-style: initial;
+        margin-left: 2em;
+    }
+</style>
