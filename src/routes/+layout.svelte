c98a8eeea57417973699a502e775c951e178713f
ajgeiss0702 <ajgeiss72@gmail.com>
2023-03-14 14:12:41 -0700

    add info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,30 +2,20 @@
   import "../app.postcss";
   import "../theme.css";
   import Header from "../lib/Header.svelte";
-  import {page} from "$app/stores";
   import { fly } from 'svelte/transition';
 </script>
 
 <style>
   .content {
-    max-width: 1200px;
-  }
-
-  .hidden-space {
-    visibility: hidden;
+    max-width: min(1200px, 95vw);
   }
 </style>
 
-{#if $page.url.pathname !== "/"}
-  <div class="fixed top-0 w-full" transition:fly={{y: -100}}>
-    <Header/>
-  </div>
-  <div class="hidden-space">
-    <Header/>
-  </div>
-{/if}
-<br>
-<div class="content mx-auto flex h-full items-center">
+
+<div class="sticky top-0 w-full" transition:fly={{y: -100}}>
+  <Header/>
+</div>
+<div class="content mx-auto flex h-full items-center p-4">
   <div>
     <slot/>
   </div>
