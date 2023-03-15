2add2b9b1760f32f23141aaa76bf425e585d3c7d
Kevin Geiss <kevin.geiss@gmail.com>
2023-03-14 17:10:29 -0700

    close p

--- src/routes/imp/+page.svelte
+++ src/routes/imp/+page.svelte
@@ -1,19 +1,21 @@
 <h1>Kevin Geiss's IMP Page</h1>
 <br>
-<p>
-<img src="/me_2553.jpg"><p>
+<br>
+<p><img src="/me_2553.jpg"></p>
 
+<p>
 The Imager for 
 <a href="http://mars.jpl.nasa.gov/default.html">Mars Pathfinder</a>
 (IMP) collected the photons you see here.
 I dithered the image to only 256 shades of gray, down from
 the 4096 the Imager is capable of, so I could convert to gif.
-<p>
+</p>
 
+<p>
 Also, the image has NOT been corrected for the electronic shutter 
 effect of the CCD. That's why the pixels on the right side of the
 image are higher in intensity than those on the left side.
-<p>
+</p>
 
 <style>
     ul {
