2a5a4512642c4dfe0a87361e7a60959d3ff8ddb6
Kevin Geiss <kevin.geiss@gmail.com>
2026-02-12 15:37:22 -0700

    updated resume

--- svelte.config.js
+++ svelte.config.js
@@ -1,32 +1,3 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-cloudflare";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
-    preprocess({
-      postcss: true,
-    }),
-  ],
-
-  kit: {
-    adapter: adapter({
-      routes: {
-        include: ["/kausyd9asda9s8dsi94hf984"], // only include random url to prevent unneeded function invocation
-        exclude: []
-      }
-    }),
-  },
-
-  version: {
-    name: (await import('child_process'))
-        .execSync('git rev-parse HEAD')
-        .toString().trim()
-  }
-};
-
-export default config;
+version https://git-lfs.github.com/spec/v1
+oid sha256:3422ad96d8e1442be2a2a7341225586b247c64aea420835a8726223e849b2680
+size 775
