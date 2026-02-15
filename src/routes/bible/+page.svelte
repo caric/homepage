b77b2b2371540699977157a0d678f15edb7b66e2
Kevin Geiss <kevin.geiss@gmail.com>
2026-02-15 13:05:33 -0700

    add transcriptions of the bible pages

--- src/routes/bible/+page.svelte
+++ src/routes/bible/+page.svelte
@@ -9,14 +9,101 @@
 <ul>
     <li><a href="/bible/img_2505.jpg" target="_blank" rel="noreferrer">Blank leaf (foxed)</a></li>
     <li><a href="/bible/img_2506.jpg" target="_blank" rel="noreferrer">Blank leaf (foxed)</a></li>
-    <li><a href="/bible/img_2507.jpg" target="_blank" rel="noreferrer">Memoranda - Cummins family births (1880s)</a></li>
-    <li><a href="/bible/img_2508.jpg" target="_blank" rel="noreferrer">Deaths - Cummins family (through 1915)</a></li>
-    <li><a href="/bible/img_2509.jpg" target="_blank" rel="noreferrer">Births - Cummins family (1890s)</a></li>
-    <li><a href="/bible/img_2510.jpg" target="_blank" rel="noreferrer">Births - Cummins family (continued)</a></li>
-    <li><a href="/bible/img_2511.jpg" target="_blank" rel="noreferrer">Marriages - Cummins family (incl. Lucile Cummins b. 1907)</a></li>
-    <li><a href="/bible/img_2512.jpg" target="_blank" rel="noreferrer">Promissory note - Chas. & Jennie Cummins, London KY (1901)</a></li>
-    <li><a href="/bible/img_2513.jpg" target="_blank" rel="noreferrer">Loose papers - "Cummins" note, Excelsior Springs clipping</a></li>
-    <li><a href="/bible/img_2514.jpg" target="_blank" rel="noreferrer">Western Union telegram - Norma's birth announcement (May 1944)</a></li>
+    <li>
+        <a href="/bible/img_2507.jpg" target="_blank" rel="noreferrer">Memoranda - Cummins family births (1880s)</a>
+        <pre>    William S. Cummins Was Bornd August 14th, 1867
+    Saly W. Cummins Was Bornd June 26th 1866
+    Ardy [or Andy?], L. Cummins Was Bornd May 22nd 1885
+    Melisie Cummins Was Bornd Jan 9th 1887
+    ESil. Cummins Was Bornd April 5th 1888
+    Jay Dottie Cummins Was " Dec 5th 1890
+    Lucille Cummins was Bornd Nov-22-1907</pre>
+    </li>
+    <li>
+        <a href="/bible/img_2508.jpg" target="_blank" rel="noreferrer">Deaths - Cummins family (through 1915)</a>
+        <pre>    Samuel C. Cummins Departed this life
+    May 21st 1840
+    James b. Cummins Departed this life
+    Aprli 6th 1858.
+    Amanda Cummins Departed this life
+    Aprile 12th 1863
+    Elisabeth Roytty[?] Departed this life
+    December 11th 1865 Age 73 yr - 10 m - 9 days
+    L. C. Cummins Departed This life
+    October 28th 1880. 11 oclock 25 am in [?]
+    Clarence C. Cummins Departed this life
+    Dec. 21, 1915.</pre>
+    </li>
+    <li>
+        <a href="/bible/img_2509.jpg" target="_blank" rel="noreferrer">Births - Cummins family (1890s)</a>
+        <pre>    Stephen Cummins was Bornd December
+    13th 1815
+    Amanda Cummins was Bornd
+    March 30th 1810.
+    Samuel C. Cummins was Bornd
+    September 11th 1832
+    William C. Cummins was Bornd
+    October 1st, 1835
+    James B. Cummins was Bornd Aprile
+    3rd 1840
+    Mary E. Cummins was Bornd July
+    15th 1843
+    Jani Bodle was Bornd January 21st 1812
+    Charles K. Cummins Was Bornd Feb 18th 1872A
+    Graydon M. Cummins was bornd April
+    27th 1893
+    Clarence. [?]. Cummins Was Bornd Jan 2
+    1895</pre>
+    </li>
+    <li><a href="/bible/img_2510.jpg" target="_blank" rel="noreferrer">Births - Cummins family (slightly different angle)</a></li>
+    <li>
+        <a href="/bible/img_2511.jpg" target="_blank" rel="noreferrer">Marriages - Cummins family (incl. Lucile Cummins b. 1907)</a>
+        <pre>    Stephen G. Cummins & Amanda Carson
+    Was United in the bonds of wedlock
+    December 27th 1831
+    Stephen G. Cummins & Jani Bodle was united
+    in the bonds of wedlock Aprile 10th 1864
+    W. S. Cummins & Saly W. Griffin Were
+    Married July 31 1884
+    Charles Cummins & Lou. Virginia McMullin
+    Was united in The bonds of wedlock 
+    July, 28th, 1892.
+    Rebecca. J. Cummins
+    Was born March 30, 1897
+    Emila. Cummins Was bornd Sept 21
+    1901
+    Mintie Marie Cummins Was Bornd Apr 11
+    1904 LUCILLE CUMMINS
+    WAS BORN NOV 22 1907</pre>
+    </li>
+    <li>
+        <a href="/bible/img_2512.jpg" target="_blank" rel="noreferrer">Promissory note - Chas. & Jennie Cummins, London KY (1901)</a>
+        <pre>    London Ky June 24
+    On or before Oct the first
+    1901 we promise to pay
+    to H. T. Hosher on order
+    sixty seven and one half
+    dollars at London Ky.
+    for value received.
+            Chas. Cummins
+            Jennie Cummins</pre>
+    </li>
+    <li>
+        <a href="/bible/img_2513.jpg" target="_blank" rel="noreferrer">Loose papers - "Cummins" note, Excelsior Springs clipping</a>
+        <pre>    Cummins
+    Note.</pre>
+    </li>
+    <li>
+        <a href="/bible/img_2514.jpg" target="_blank" rel="noreferrer">Western Union telegram - Norma's birth announcement (May 1944)</a>
+        <pre>    FA121 8=AU LAPLATA MO 4 330P     1944 MAY 4 PM 2 30
+    R N CUMMINS Y1/C USNA=
+    349 SOUTH VANNESS AVE APT 5 SFRAN=
+
+    BABY GIRL BORN 8 OCLOCK TODAY BOTH FINE.=
+                            MOTHER.
+
+                            8</pre>
+    </li>
     <li><a href="/bible/img_2515.jpg" target="_blank" rel="noreferrer">Back cover - embossed leather</a></li>
     <li><a href="/bible/img_2516.jpg" target="_blank" rel="noreferrer">Illustration - "Boaz and Ruth"</a></li>
     <li><a href="/bible/img_2517.jpg" target="_blank" rel="noreferrer">Title page - "The Holy Bible" (colored illustration)</a></li>
@@ -25,8 +112,14 @@
     <li><a href="/bible/img_2520.jpg" target="_blank" rel="noreferrer">Back cover (overhead view)</a></li>
     <li><a href="/bible/img_2521.jpg" target="_blank" rel="noreferrer">Back cover (showing wear)</a></li>
     <li><a href="/bible/img_2522.jpg" target="_blank" rel="noreferrer">Back cover (corner damage)</a></li>
-    <li><a href="/bible/img_2523.jpg" target="_blank" rel="noreferrer">Inside back cover - pencil inscription</a></li>
-    <li><a href="/bible/img_2524.jpg" target="_blank" rel="noreferrer">Inside back cover - pencil inscription (detail)</a></li>
+    <li>
+        <a href="/bible/img_2523.jpg" target="_blank" rel="noreferrer">Inside back cover - pencil inscription</a>
+        <pre>    Graydon Cummins</pre>
+    </li>
+    <li>
+        <a href="/bible/img_2524.jpg" target="_blank" rel="noreferrer">Inside back cover - pencil inscription (slightly different angle)</a>
+        <pre>    Graydon Cummins</pre>
+    </li>
     <li><a href="/bible/img_2525.jpg" target="_blank" rel="noreferrer">Spine - worn leather binding</a></li>
     <li><a href="/bible/img_2526.jpg" target="_blank" rel="noreferrer">Spine (alternate angle)</a></li>
     <li><a href="/bible/img_2527.jpg" target="_blank" rel="noreferrer">Spine (alternate angle)</a></li>
@@ -37,5 +130,11 @@
     ul {
         list-style: initial;
         margin-left: 2em;
+    }
+    pre {
+        margin: 0.25em 0 0.5em 0;
+        font-size: 0.85em;
+        white-space: pre-wrap;
+        opacity: 0.85;
     }
 </style>
