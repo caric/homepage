08b7b93c100e35a1f413f326b04882f99ea6c9c2
Kevin Geiss <kevin.geiss@gmail.com>
2026-06-19 14:19:40 -0700

    add calculator

---
+++ static/fx7500g/emulator.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:67a442c82caaa00c794e60cc69e53e7048fdb612c62b2320f1475e62dcea908e
+size 50419
---
+++ static/fx7500g/ui.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9b8878f633790413d09e484e5a0a1c377a5441d02e0f79451dbef17a978b957
+size 11341
