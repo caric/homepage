884fb17c68f90e47af973c7ca6b481bb72069c71
Kevin Geiss <kevin.geiss@gmail.com>
2026-08-09 09:47:09 -0700

    Sync VelaClock: add "Pick a Time" feature

--- static/velaclock/js/app.js
+++ static/velaclock/js/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6d2f9d508d3af83ec0db3d27187af0f3e3cf90bc6e813d96a7fddc786d7adc8
-size 9912
+oid sha256:a6610bb261b11fe83c6e9d6f6a76ebedb5a3a6635a8fc4e811ca542370d3ecf8
+size 12559
